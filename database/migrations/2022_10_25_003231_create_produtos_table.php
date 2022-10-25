d9ed5467e07bf4fed5a3add8f9ec0afacb1d2780
dev-leonzera <leonandrade22@gmail.com>
2022-10-24 22:09:16 -0300

    First commit

---
+++ database/migrations/2022_10_25_003231_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('loja_id')->constrained('lojas')->onDelete('cascade');
+            $table->string('nome', 60);
+            $table->double('valor', 6, 2);
+            $table->boolean('ativo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
